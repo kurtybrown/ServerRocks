22acda283280623a03ee0a77c76a865585787d82
kurtybrown <carlosocana91@hotmail.com>
2021-05-21 20:18:13 +0200

    añadido cors

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const express = require ('express');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const app = express();
+app.use(cors()); //nos permite aceptar otras peticiones que vienen de otros servidores ej: 4200 con 3000
 app.use(morgan('dev')); //para que el servidor se ejecute cada vez que realizo cambios
 app.use(express.json()); //entender los json que envío por postman
 app.use(express.urlencoded({extended:false}));
