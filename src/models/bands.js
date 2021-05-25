6ba306e646707d1987800fba8f1027fa3dee8642
kurtybrown <carlosocana91@hotmail.com>
2021-05-25 12:02:00 +0200

    ajustes

--- src/models/bands.js
+++ src/models/bands.js
@@ -4,10 +4,10 @@ const bandSchema = new Schema({
 
     name:{type: String, required : true},
     foundation:{type: Number, required : true},
-    members:{type: Array, required : true},
-    styles:{type: Array, required : true},
-    song:{type: String, required : true},
-    picture: {type: String, required:true}
+    members:{type: String, required : true},
+    styles:{type: String, required : true},
+    song:{type: String, },
+    photo: {type: String,}
 },
 {
     timestamps: true, //muestra última vez que se actualizó
