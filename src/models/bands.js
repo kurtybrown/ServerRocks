241ff5f21a91377296c8206bab0c342d41387336
kurtybrown <carlosocana91@hotmail.com>
2021-05-25 13:27:06 +0200

    requires

--- src/models/bands.js
+++ src/models/bands.js
@@ -2,10 +2,12 @@ const {Schema, model} = require('mongoose');
 
 const bandSchema = new Schema({
 
-    name:{type: String, required : true},
-    foundation:{type: Number, required : true},
-    members:{type: String, required : true},
-    styles:{type: String, required : true},
+    // required : true
+
+    name:{type: String, },
+    foundation:{type: Number, },
+    members:{type: String, },
+    styles:{type: String, },
     song:{type: String, },
     photo: {type: String,}
 },
